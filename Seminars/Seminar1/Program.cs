b9b73c6560768cc5ebcb4310b222d60464534161
Azamat <sch_azamat@mail.ru>
2023-04-20 23:23:17 +0500

    Задачи к семинару 1

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"You input {num} number");
+
+
+// Task1. Запросить число и вывести его квадрат.
+// Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Quad of {num} -> {num*num}");
+
+
+// Task2. Является ли первое число квадратом второго
+// Console.WriteLine("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2 * num2)
+// {
+//     Console.WriteLine($"{num1} является квадратом {num2}");
+// }
+// else
+// {
+// Console.WriteLine($"{num1} не является квадратом {num2}");
+// }
+
+
+// Task3. Напишите программу, которая принимает на вход число n,
+// а на выходе получаем последовательность от -n до n
+// 
+
+
+// Task4. На вход принимаем трехзначное число. Вывести последнюю цифру данного числа
+Console.WriteLine("Input Three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int ed = n % 10;
+Console.WriteLine($"Last digit of {n} -> {ed}");
