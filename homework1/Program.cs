380bde3d0ecef5cd904e65a4c0d6f3edf4128187
Azamat <sch_azamat@mail.ru>
2023-04-22 19:10:37 +0500

    Task 4. Max of three numbers

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -13,3 +13,26 @@
 // {
 //     Console.WriteLine($"Max number: {num2}");
 // }
+
+
+//Task2. Напишите программу, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int maximum = num1;
+// if (num2 > maximum)
+// {
+//     maximum = num2;
+// }
+// if (num3 > maximum)
+// {
+//     maximum = num3;
+// }
+// Console.WriteLine($"Max number: {maximum}");
+
+
